bfac6e96bd3ec75b534edbafba45c92705952da0
shabbir <shabbirbadshah128@gmail.com>
2018-02-23 17:17:27 +0530

    postgrace

--- db.js
+++ db.js
@@ -1,8 +1,18 @@
 var Sequelize = require("sequelize");
-sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/todo-data.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if (env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
+} else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/todo-data.sqlite'
+	});
+
+}
 
 var db = {}
 
@@ -13,6 +23,3 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
-
-
-
